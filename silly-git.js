34588b5ffe4c4a088de1dba1f139b81149c610d1
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2022-11-07 16:02:14 -0500

    silly git demo

--- silly-git.js
+++ silly-git.js
@@ -1,1 +1,9 @@
 const faveFruit = 'strawberry';
+function worstFruit(nameOfFruit){
+    if(nameOfFruit.toLowerCase()==='banana'||nameOfFruit.toLowerCase()==='bananas'){
+        return `Yes, ${nameOfFruit} is the worst fruit`
+    }
+    else {
+        return `No, ${nameOfFruit} is better than bananas. Bananas are the worst fruit.`
+    }
+}
